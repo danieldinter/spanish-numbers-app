46320de87f0caaa6c24a3cefb3d66182114f55da
dani <danieldinter@users.noreply.github.com>
2025-10-24 17:45:00 +0200

    Fix inverse mode toggle in dark mode

--- src/App.vue
+++ src/App.vue
@@ -145,7 +145,7 @@ function checkAnswer() {
             v-model="isReverseMode"
           />
           <div
-            class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-indigo-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600 dark:peer-checked:bg-indigo-600"
+            class="relative w-11 h-6 bg-gray-200 dark:bg-gray-400 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-indigo-300 dark:peer-focus:ring-indigo-800 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600 dark:peer-checked:bg-indigo-600"
           ></div>
           <span class="ms-3 text-gray-900 dark:text-white">inverso</span>
         </label>
